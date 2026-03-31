3531e732a20fbeabbaefca6ac172f78ab9ccce8b
David202151 <davidalejandrovargas20@gmail.com>
2026-03-31 12:26:21 -0500

    Solucion del tercer ejercicio de leet code

--- 3. Longest Substring Without Repeating Characters/lengthOfLongestSubstring.py
+++ 3. Longest Substring Without Repeating Characters/lengthOfLongestSubstring.py
@@ -2,8 +2,16 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         max_len = 0 
         for i in range(len(s)):
-            print(i)
-        
+            seen = set()
+            contador = 0 
+            for j in range(i, len(s)):
+                if s[j] in seen:
+                    break
+                contador += 1
+                seen.add(s[j])
+                max_len = max(max_len, contador)
+        return(max_len)
+                    
         
 sol = Solution()
 sol.lengthOfLongestSubstring('abcabcbb')
